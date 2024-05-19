2e80145fb2b7adec57f00ed3f44efd07f244c352
muendokennedy <kennedymunyao999@gmai.com>
2024-05-19 16:17:44 +0300

    Created the counter, Customer, Invoice and Product models together with their respective factories and migrations

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,6 +2,10 @@
 
 namespace Database\Seeders;
 
+use App\Models\Counter;
+use App\Models\Customer;
+use App\Models\Invoice;
+use App\Models\Product;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
@@ -15,9 +19,14 @@ class DatabaseSeeder extends Seeder
     {
         // User::factory(10)->create();
 
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+//        User::factory()->create([
+//            'name' => 'Test User',
+//            'email' => 'test@example.com',
+//        ]);
+
+//        Counter::factory(1)->create();
+//        Product::factory(5)->create();
+//        Customer::factory(50)->create();
+        Invoice::factory(100)->create();
     }
 }
